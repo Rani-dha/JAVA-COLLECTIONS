45e85dd564885b87f095273b816dd6f305ec5aa8
Dharani T <gct.it18.dharani@gmail.com>
2021-02-19 17:34:40 +0530

    Start coding in Java

---
+++ startCoding.java
@@ -0,0 +1,22 @@
+class Geeks{
+    
+    // Function to print hello
+    static void printHello(){
+        
+        // Your code here
+        System.out.print("Hello World");
+        
+    }    
+}
+
+class startCoding{
+    public static void main(String args[]){
+       
+       //Creating an Object of Class Geeks
+        Geeks g = new Geeks();
+        
+        //Calling printHello() function of the Class Geeks
+        g.printHello();
+    }
+    
+}  
