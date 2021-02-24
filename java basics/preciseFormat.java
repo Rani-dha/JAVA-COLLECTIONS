ccdbfdd3c4a5a44e7c438984765c4875b3fa9ddd
Dharani T <gct.it18.dharani@gmail.com>
2021-02-24 22:29:10 +0530

    files

---
+++ java basics/preciseFormat.java
@@ -0,0 +1,10 @@
+class Geeks{
+    
+    static void printInFormat(float a, float b){
+        float result = a/b;
+        System.out.printf("%.7f", result);
+        System.out.printf(" %.3f", result);
+        System.out.println();       
+    }    
+    
+}
