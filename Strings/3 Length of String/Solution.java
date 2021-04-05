be8a729bbeb61b521ac9cdb15ae0aed763f9c85a
Dharani T <gct.it18.dharani@gmail.com>
2021-04-05 22:04:45 +0530

    length()

---
+++ Strings/3 Length of String/Solution.java
@@ -0,0 +1,13 @@
+// Program 3: https://practice.geeksforgeeks.org/problems/length-of-string/1/
+
+class Solution
+{
+    // Complete the function
+    // str: input string
+    public static int lengthString(String str)
+    {
+        // find the length of string
+            return str.length();
+
+    }
+}
