142ec976ae49f4d2da7f47aff75835e82620b8c5
Dharani T <gct.it18.dharani@gmail.com>
2021-05-07 22:05:00 +0530

    remove consecutive duplicates

--- Lists/Stack/4 Removing consecutive duplicates/GFG.java
+++ Lists/Stack/4 Removing consecutive duplicates/GFG.java
@@ -42,5 +42,31 @@ class Solution
             }
         }
         return res;
+
+
+        // GfG Solution
+        /* 
+        Stack<Character> st = new Stack<Character>();
+		
+		//iterating over the string.
+		for(int i=0; i<str.length(); i++)
+		{
+		    //if current character of string is not equal to the character at  
+            //top of stack, we push the current character into the stack.
+            if(st.empty() || st.peek()!=str.charAt(i))
+            {
+              st.push(str.charAt(i));
+            }
+		 }
+		 String res = new String("");
+		 
+		 //adding all characters in the stack to the answer and returning it.
+		 while(!st.empty())
+		 {
+		    res = st.peek() + res;
+		    st.pop();
+		 }
+	     return(res);
+        */
     }
 }
