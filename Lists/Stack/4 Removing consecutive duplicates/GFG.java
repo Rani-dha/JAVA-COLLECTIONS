b03c5d259c8bda03fe0d3d01f939275df71d3e8f
Dharani T <gct.it18.dharani@gmail.com>
2021-05-07 22:02:30 +0530

    remove consecutive duplicates

--- Lists/Stack/4 Removing consecutive duplicates/GFG.java
+++ Lists/Stack/4 Removing consecutive duplicates/GFG.java
@@ -1,5 +1,5 @@
-//
-//
+// Program 4: Removing consecutive duplicates
+// https://practice.geeksforgeeks.org/problems/removing-consecutive-duplicates-1587115621/1/?track=Java-Collections-Stack&batchId=318#
 
 import java.util.*;
 import java.lang.*;
