de419f992cc6669b0d882f9a8f5ed605f288cc8b
Dharani T <gct.it18.dharani@gmail.com>
2021-05-18 21:51:29 +0530

    Queue

---
+++ Queue/Queue/5 Queue using two stacks/GfG.java
@@ -0,0 +1,120 @@
+// Program 5: Queue using two stacks
+// https://practice.geeksforgeeks.org/problems/queue-using-two-stacks/1/?track=Java-Collections-Queue&batchId=318#
+
+import java.util.*;
+import java.util.Stack;
+import java.util.LinkedList;
+
+
+
+class GfG
+{
+	public static void main(String args[])
+	{
+	    //Taking input using class Scanner
+		Scanner sc = new Scanner(System.in);
+		
+		//Taking input the number of testcases
+		int t = sc.nextInt();
+		while(t>0)
+		{
+		    //Creating a new object of class StackQueue
+			StackQueue g = new StackQueue();
+			
+			//Taking input the total number of Queries
+			int q = sc.nextInt();
+			while(q>0)
+			{
+				int QueryTyoe = sc.nextInt();
+				
+				//If QueryTyoe is 1 then
+				//we call the Push method
+				//of class StackQueue
+				//else we call the Pop method
+				if(QueryTyoe == 1)
+				{
+					int a = sc.nextInt();
+					g.Push(a);
+				}else
+				if(QueryTyoe == 2)
+				System.out.print(g.Pop()+" ");
+			q--;
+				
+			}
+			System.out.println();
+		t--;
+		}
+	}
+}
+
+// } Driver Code Ends
+
+
+
+class StackQueue
+{
+    Stack<Integer> s1 = new Stack<Integer>();
+    Stack<Integer> s2 = new Stack<Integer>();
+
+    //Function to push an element in queue by using 2 stacks.
+    void Push(int x)
+    {
+	   // Your code here
+       // Move all elements from s1 to s2 
+	  while( !s1.isEmpty())
+           s2.push(s1.pop());
+	   
+	   s1.push(x);  // Push the element into s1
+	   
+	   while( !s2.isEmpty()) // Push everything back to s1 
+	       s1.push(s2.pop());
+    }
+	
+    
+    //Function to pop an element from queue by using 2 stacks.
+    int Pop()
+    {   // Your code here
+        if( s1.isEmpty() && s2.isEmpty())
+          return -1;
+
+        return  s1.pop();
+    }
+}
+
+/* 
+
+Example 1:
+
+Input:
+5
+1 2 1 3 2 1 4 2
+
+Output: 
+2 3
+
+Explanation: 
+In the first testcase
+1 2 the queue will be {2}
+1 3 the queue will be {2 3}
+2   poped element will be 2 the queue 
+    will be {3}
+1 4 the queue will be {3 4}
+2   poped element will be 3.
+Example 2:
+
+Input:
+4
+1 2 2 2 1 4
+
+Output: 
+2 -1
+
+Explanation: 
+In the second testcase 
+1 2 the queue will be {2}
+2   poped element will be 2 and 
+    then the queue will be empty
+2   the queue is empty and hence -1
+1 4 the queue will be {4}.
+
+*/
