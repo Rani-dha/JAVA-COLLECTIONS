ea7138840d1ef90e9a8750ae06816a10a9fbf4a0
Dharani T <gct.it18.dharani@gmail.com>
2021-06-01 21:37:07 +0530

    Rotate deque

---
+++ Queue/Deque(Double ended queue)/4 Rotate deque by k elements/GfG.java
@@ -0,0 +1,81 @@
+// Program 4: Rotate deque by K elements
+// https://practice.geeksforgeeks.org/problems/rotate-deque-by-k/1/?track=Java-Collections-Deque&batchId=318#
+
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking count of all testcases
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		     //Declaring and Initializing an ArrayDeque
+		     ArrayDeque<Integer> deque = new ArrayDeque<Integer>(); 
+		     
+		     //Taking the count of total elements
+		     int n=sc.nextInt();
+		    
+		     //adding all the elements to ArrayDeque 
+		     for(int i=0;i<n;i++)
+		     {
+		         int x=sc.nextInt();
+		         deque.add(x);
+		     }
+		     
+		     int rotateQuery, k;
+		     
+		     //Taking type of rotation
+		     rotateQuery=sc.nextInt();
+		     //number of rotations
+		     k=sc.nextInt();
+		     
+		     //if rotation type is 1 we call the method right_Rotate_Deq_ByK
+		    if(rotateQuery == 1)
+		        right_Rotate_Deq_ByK(deque, n, k);
+                
+            else//else we call the method left_Rotate_Deq_ByK
+                left_Rotate_Deq_ByK(deque, n, k);
+            
+            //printing all the elements of the ArrayDeque
+            for(int itr:deque)
+                System.out.print(itr+" ");
+            System.out.println();
+		}
+	}
+	
+	
+ // } Driver Code Ends
+//User function Template for Java
+
+
+//Function to rotate deque by k places in anti-clockwise direction.
+public static void left_Rotate_Deq_ByK(ArrayDeque<Integer> deque, int n, int k)
+{
+    //Your code here
+    for(int i=0; i<k;i++)
+       deque.offerLast(deque.pollFirst());
+}
+
+//Function to rotate deque by k places in clockwise direction.
+public static void right_Rotate_Deq_ByK(ArrayDeque<Integer> deque, int n, int k)
+{
+    //Your code here
+    for(int i=0;i<k;i++){
+        deque.offerFirst(deque.pollLast());
+    }
+}
+
+// { Driver Code Starts.
+
+
+}  // } Driver Code Ends
