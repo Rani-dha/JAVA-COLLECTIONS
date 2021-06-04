8b1940409da8ecf385a09fd5bf13798ae26744c0
Dharani T <gct.it18.dharani@gmail.com>
2021-06-04 22:22:19 +0530

    HashSet union

--- Sets/HashSet and LinkedHashSet/5 Union of two arrays/GfG.java
+++ Sets/HashSet and LinkedHashSet/5 Union of two arrays/GfG.java
@@ -73,3 +73,16 @@ class Solution{
         return unionCount;
     }
 }
+
+// Input:
+// 5 3
+// 1 2 3 4 5
+// 1 2 3
+
+// Output: 
+// 5
+
+// Explanation: 
+// 1, 2, 3, 4 and 5 are the
+// elements which comes in the union set
+// of both arrays. So count is 5.
